2993c53af0599f4dbdc376b0dd4b6a42f9c5d5ae
kriplegrand <sasvbmgk@gmail.com>
2025-09-17 11:10:09 +0200

    Update AccessExpiredPage.jsx

--- Codeappli-upload/src/pages/AccessExpiredPage.jsx
+++ Codeappli-upload/src/pages/AccessExpiredPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { motion } from 'framer-motion';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext.jsx';
 import { useNavigate } from 'react-router-dom';
 import { Clock, Mail } from 'lucide-react';
 
