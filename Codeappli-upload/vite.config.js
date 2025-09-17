605bedbdedefb00f7b0d56cc79e9a5204d8ec05d
kriplegrand <sasvbmgk@gmail.com>
2025-09-17 12:10:05 +0200

    Update vite.config.js

--- Codeappli-upload/vite.config.js
+++ Codeappli-upload/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Pour les assets (CSS, images, etc.)
+  resolve: {   // Pour l'alias @/ dans vos imports
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'dist'
   }
