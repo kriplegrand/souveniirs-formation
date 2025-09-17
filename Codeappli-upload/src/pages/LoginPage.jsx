90bcbdd906230fc8c3a91fd94180c4ddbd78a4ce
kriplegrand <sasvbmgk@gmail.com>
2025-09-17 11:12:31 +0200

    Update LoginPage.jsx

--- Codeappli-upload/src/pages/LoginPage.jsx
+++ Codeappli-upload/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { motion } from 'framer-motion';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext.jsx';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
