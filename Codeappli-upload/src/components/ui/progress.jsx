802ed5734fc2e7bcb9cf89b15f70fd6497e5e1fd
kriplegrand <sasvbmgk@gmail.com>
2025-09-17 11:19:47 +0200

    Update progress.jsx

--- Codeappli-upload/src/components/ui/progress.jsx
+++ Codeappli-upload/src/components/ui/progress.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils.js'
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
